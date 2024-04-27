95477bd557a286ad1343e815a9b562a868bd4225
PSepulvedaS <pablo.seplveda@uc.cl>
2024-04-26 20:27:36 -0400

    Changed config.js file to only use development db

--- src/config/config.js
+++ src/config/config.js
@@ -16,30 +16,30 @@ module.exports = {
       }
     }
   },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_test`,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  },
+  // test: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: `${process.env.DB_NAME}_test`,
+  //   host: process.env.DB_HOST,
+  //   dialect: "postgres",
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false
+  //     }
+  //   }
+  // },
+  // production: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: `${process.env.DB_NAME}_production`,
+  //   host: process.env.DB_HOST,
+  //   dialect: "postgres",
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false
+  //     }
+  //   }
+  // },
 };
